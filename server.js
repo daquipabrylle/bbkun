91659522d941bc14aebf102ab2705f61e85ae662
daquipabrylle <daquipab@gmail.com>
2017-03-19 04:37:35 +0800

    brylle

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
-//require modules
+
 var express = require('express');
 var path = require('path');
-//instantiate express
+
 var app = express();
-//set port
-app.set('port', (process.env.PORT || 5000));
-//use static files
+
+app.set('port', (process.env.PORT || 9000));
+
 app.use(express.static(path.join(__dirname, 'public')));
-//express routes
+
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'HOMEPAGE.html'));
 });
-app.get('/part1', function(req, res){
+app.get('/app1', function(req, res){
   res.sendFile(path.join(__dirname, 'app1.html'));
 });
-app.get('/part2', function(req, res){
+app.get('/app2', function(req, res){
   res.sendFile(path.join(__dirname, 'app2.html'));
 });
-app.get('/part3', function(req, res){
+app.get('/app3', function(req, res){
   res.sendFile(path.join(__dirname, 'app3.html'));
 });
-app.get('/part4', function(req, res){
+app.get('/app4', function(req, res){
   res.sendFile(path.join(__dirname, 'app4.html'));
 });
-app.get('/part5', function(req, res){
+app.get('/app5', function(req, res){
   res.sendFile(path.join(__dirname, 'app.html'));
 });
-//express server listen
+app.get('*', function(req, res){
+  res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
+});
+
 var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port ',app.get('port'));
 });
