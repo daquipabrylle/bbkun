345c9568a721c1cfe74c37c5b1637e0d7ed698a9
daquipabrylle <daquipab@gmail.com>
2017-03-19 14:29:39 +0800

    brylle

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
+app.get('/HOMEPAGE', function(req, res){
   res.sendFile(path.join(__dirname, 'views/HOMEPAGE.html'));
 });
 app.get('/app1', function(req, res){
